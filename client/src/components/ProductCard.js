fb84d359ccebddcbf26ba5da546e791057be4399
Lucas Oliveira <lucasnoliveira126@gmail.com>
2024-02-22 15:44:18 -0300

    bug fixes

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -11,7 +11,7 @@ const ProductCard = ({ product, addToCart }) => {
     };
 
     return (
-        <Card variant="outlined" style={{ width: '300px'}}>
+        <Card variant="outlined">
             {/* Imagem */}
             <img src={product.image} alt={product.name} style={{ width: '100%', height: '350px' }} />
             <CardContent>
--- client/src/components/ProductGrid.js
+++ client/src/components/ProductGrid.js
@@ -4,17 +4,16 @@ import ProductCard from './ProductCard';
 
 const ProductGrid = ({ products, addToCart }) => {
     return (
-        <div style={{ position: 'absolute', right: '1rem', left: '20%' }}>
-            <Grid container spacing={0}>
-                {products.map(product => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        {/* Passe a função addToCart como propriedade para o componente ProductCard */}
-                        <ProductCard product={product} addToCart={addToCart} />
-                    </Grid>
-                ))}
-            </Grid>
-        </div>
+        <Grid container spacing={2} className="product-grid-container" sx={{ marginTop: '20px' }}>
+            {products.map(product => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    {/* Passe a função addToCart como propriedade para o componente ProductCard */}
+                    <ProductCard product={product} addToCart={addToCart} />
+                </Grid>
+            ))}
+        </Grid>
     );
 };
 
 export default ProductGrid;
+
