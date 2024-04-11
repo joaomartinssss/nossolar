63d0d5b75854d14c8c57b62d8e001c7ca48cd1b6
lucasnoliveira <lucasnoliveira126@gmail.com>
2024-04-11 16:32:56 -0300

    botão rota categorias

--- client/src/components/Categoria.js
+++ client/src/components/Categoria.js
@@ -1,22 +1,8 @@
-// import React from "react";
-
-// const colunaEsquerda = () => {
-//     const estiloDaColuna = {
-
-//     }
-// }
-
 import React from "react";
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const Categoria = () => {
   const style = {
-    //   root: {
-    //     '--amarelo-principal': '#f5e37f',
-    //     '--azul-principal': '#003566',
-    //     '--vermelho-opcional': '#e74c3c',
-    //     '--amarelo-secundario': '#ffc300',
-    //     '--header-height': '120px',
-    //   },
     produtos: {
       display: "block",
       width: "300px",
@@ -30,56 +16,55 @@ const Categoria = () => {
       borderRadius: "5px",
       padding: "20px",
     },
+    buttonContainer: {
+      width: "100%",
+      marginBottom: "0.5rem",
+    },
     button: {
-      flexDirection: "column",
+      display: "block",
       borderRadius: "5px",
       justifyContent: "center",
       alignItems: "center",
       backgroundColor: "#003599",
       color: "aliceblue",
       padding: "0.3rem 1rem",
-      marginTop: "0.5rem",
       fontSize: "1rem",
       cursor: "pointer",
-      border: "none",
-      width: "100%",
+      textDecoration: "none",
     },
     buttonHover: {
-      flexDirection: "column",
-      borderRadius: "5px",
-      justifyContent: "center",
-      alignItems: "center",
       backgroundColor: "rgb(0, 191, 255)",
-      color: "#003599",
       fontWeight: "bold",
-      width: "100%",
     },
   };
+
   return (
     <section id="produtos" style={style.produtos}>
       <h2>Nossos Produtos</h2>
       <div className="botao-dos-produtos">
-        <button style={style.button}>Hortifruti</button>
-        <button style={style.button}>Padaria</button>
-        <button style={style.button}>Açougue</button>
-        <button style={style.button}>Bebidas</button>
-        <button style={style.button}>Rotisseria</button>
-        <button style={style.button}>Bomboniere</button>
-        <button style={style.button}>Bazar</button>
-        <button style={style.button}>Automotivo</button>
-        <button style={style.button}>Mercearia</button>
-        <button style={style.button}>Limpeza</button>
-        <button style={style.button}>Laticínios</button>
-        <button style={style.button}>Bebês</button>
-        <button style={style.button}>Higiene</button>
-        <button style={style.button}>Congelados</button>
-        <button style={style.button}>Japonês</button>
-        <button style={style.button}>Churrasco</button>
-        <button style={style.button}>Doces</button>
-        <button style={style.button}>Utilidades</button>
-        <button style={style.button}>Frios</button>
-        <button style={style.button}>Petshop</button>
-        <button style={style.button}>Utilidades</button>
+      <Router>
+          <Link to="/hortifruti" style={style.button}>Hortifruti</Link>
+          <Link to="/padaria" style={style.button}>Padaria</Link>
+          <Link to="/acougue" style={style.button}>Açougue</Link>
+          <Link to="/bebidas" style={style.button}>Bebidas</Link>
+          <Link to="/rotisseria" style={style.button}>Rotisseria</Link>
+          <Link to="/bomboniere" style={style.button}>Bomboniere</Link>
+          <Link to="/bazar" style={style.button}>Bazar</Link>
+          <Link to="/automotivo" style={style.button}>Automotivo</Link>
+          <Link to="/mercearia" style={style.button}>Mercearia</Link>
+          <Link to="/limpeza" style={style.button}>Limpeza</Link>
+          <Link to="/laticinios" style={style.button}>Laticínios</Link>
+          <Link to="/bebes" style={style.button}>Bebês</Link>
+          <Link to="/higiene" style={style.button}>Higiene</Link>
+          <Link to="/congelados" style={style.button}>Congelados</Link>
+          <Link to="/japones" style={style.button}>Japonês</Link>
+          <Link to="/churrasco" style={style.button}>Churrasco</Link>
+          <Link to="/doces" style={style.button}>Doces</Link>
+          <Link to="/utilidades" style={style.button}>Utilidades</Link>
+          <Link to="/frios" style={style.button}>Frios</Link>
+          <Link to="/petshop" style={style.button}>Petshop</Link>
+        </Router>
+
       </div>
     </section>
   );
