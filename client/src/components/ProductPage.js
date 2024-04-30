145191dd27e6dafc1fbd601abcd2f614ed625eb5
João Martins <jvhotmart0@gmail.com>
2024-04-29 23:33:20 -0300

    update backButton product page

--- client/src/components/ProductPage.js
+++ client/src/components/ProductPage.js
@@ -1,59 +1,75 @@
-import React from 'react';
-import { makeStyles } from '@mui/styles';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const useStyles = makeStyles({
   productPage: {
-    display: 'flex',
+    display: "flex",
     // alignItems: 'center',
     // justifyContent: 'center',
-    width: '100%',
+    width: "100%",
     // background: "green"
   },
   productContainer: {
-    display: 'flex',
-    marginLeft: '3rem',
-    alignItems: 'center',
+    display: "flex",
+    marginLeft: "3rem",
+    alignItems: "center",
     // backgroundColor: 'yellow', // Set a background color for the container
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     // flexDirection: 'row', // Align items horizontally
-    maxWidth: '800px', // Adjust the maximum width if needed
+    maxWidth: "800px", // Adjust the maximum width if needed
   },
   productImage: {
-    width: '400px', // Set a fixed width for the image
-    marginRight: '20px', // Adjust the margin to create space between the image and details
+    width: "400px", // Set a fixed width for the image
+    marginRight: "20px", // Adjust the margin to create space between the image and details
   },
   productDetails: {
     flex: 1, // Allow the details to grow to fill available space
-    textAlign: 'left', // Align text to the left
+    textAlign: "left", // Align text to the left
   },
 });
 
-const ProductPage = ({ product }) => {
+const ProductPage = ({ product, history }) => {
   const classes = useStyles();
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
 
   return (
     <div className={classes.productPage}>
       <Card>
         <CardContent className={classes.productContainer}>
+          <ArrowBackIcon>
+            <button onClick={handleGoBack} style={{}}></button>
+          </ArrowBackIcon>
           <div className={classes.productDetails}>
-            <img src={product.image} alt={product.name} className={classes.productImage}  style={{border: "solid 5px #D8DDDE"}}/>
+            <img
+              src={product.image}
+              alt={product.name}
+              className={classes.productImage}
+              style={{ border: "solid 5px #D8DDDE" }}
+            />
           </div>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
             <Typography variant="h4" gutterBottom>
               {product.name}
             </Typography>
-            <Typography variant="body1" gutterBottom style={{ fontSize: '2.5rem', fontWeight: '700', color: '#333' }}>
+            <Typography
+              variant="body1"
+              gutterBottom
+              style={{ fontSize: "2.5rem", fontWeight: "700", color: "#333" }}
+            >
               R$ {product.price.toFixed(2)}
             </Typography>
-            <Button variant="contained" style={{backgroundColor: "gold"}}>
+            <Button variant="contained" style={{ backgroundColor: "gold" }}>
               Adicionar ao Carrinho
             </Button>
           </div>
-
         </CardContent>
       </Card>
     </div>
