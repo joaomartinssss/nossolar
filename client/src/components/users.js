adbab99205c2008de132e6b00ddc7242f8734f55
João Martins <jvhotmart0@gmail.com>
2024-06-19 20:37:55 -0300

    update users.js

--- client/src/components/users.js
+++ client/src/components/users.js
@@ -1,56 +1,72 @@
-import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { styled } from '@mui/system';
-import { Button, Typography, Avatar } from '@mui/material';
-import PersonIcon from '@mui/icons-material/Person';
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { styled } from "@mui/system";
+import { Button, Typography, Avatar } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
 
-const StyledUsersContainer = styled('div')({
-    position: 'absolute',
-    top: '450%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: 'white',
-    padding: '2rem',
-    borderRadius: '8px',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-    textAlign: 'center',
+const StyledUsersContainer = styled("div")({
+  position: "absolute",
+  top: "410%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "white",
+  padding: "2rem 3rem",
+  borderRadius: "8px",
+  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+  textAlign: "center",
 });
 
-const StyledButtonContainer = styled('div')({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginTop: '1rem',
+const StyledButtonContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "1rem",
+});
+const StyledTypography = styled(Typography)({
+  whiteSpace: "nowrap",
+  color: "black",
 });
 
-const Users = ({ handleClose }) => {
-    return (
-        <Router>
-            <StyledUsersContainer>
-                <Avatar sx={{ bgcolor: 'primary.main', width: 64, height: 64, margin: '0 auto 1rem' }}>
-                    <PersonIcon sx={{ fontSize: 48 }} />
-                </Avatar>
-                <Typography variant="h6" gutterBottom style={{color: 'black'}}>
-                    Seja Bem-vindo!
-                </Typography>
-                <StyledButtonContainer>
-                    <Link to="/login">
-                        <Button variant="outlined" color="primary" style={{ marginBottom: '0.5rem' }}>
-                            Entrar
-                        </Button>
-                    </Link>
-                    <Link to="/cadastro">
-                        <Button variant="outlined" color="primary" style={{ marginBottom: '0.5rem' }}>
-                            Cadastrar
-                        </Button>
-                    </Link>
-                    {/* <Button variant="contained" color="primary" onClick={handleClose}>
-                        Fechar
-                    </Button> */}
-                </StyledButtonContainer>
-            </StyledUsersContainer>
-        </Router>
-    );
+const Users = ({}) => {
+  return (
+    <Router>
+      <StyledUsersContainer>
+        <Avatar
+          sx={{
+            bgcolor: "primary.main",
+            width: 45,
+            height: 45,
+            margin: "0 auto 1rem",
+          }}
+        >
+          <PersonIcon sx={{ fontSize: 35 }} />
+        </Avatar>
+        <StyledTypography variant="h6" style={{ color: "black" }}>
+          Seja Bem-vindo!
+        </StyledTypography>
+        <StyledButtonContainer>
+          <Link to="/login">
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginBottom: "0.5rem", minWidth: "150px" }}
+            >
+              Entre
+            </Button>
+          </Link>
+          <Link to="/cadastro">
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginBottom: "0.5rem", minWidth: "150px" }}
+            >
+              Cadastre-se
+            </Button>
+          </Link>
+        </StyledButtonContainer>
+      </StyledUsersContainer>
+    </Router>
+  );
 };
 
 export default Users;
