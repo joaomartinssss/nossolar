19851cb5ab4b802e929b516e88140a3affff0f2e
João Martins <jvhotmart0@gmail.com>
2024-07-28 00:41:20 -0300

    responsividade ProductGrid

--- client/src/components/ProductGrid.js
+++ client/src/components/ProductGrid.js
@@ -1,20 +1,50 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import ProductCard from './ProductCard';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import ProductCard from "./ProductCard";
+import { useMediaQuery } from "@mui/material";
+import breakPoints from "./BreakPoints";
 
-const ProductGrid = ({ products, addToCart, onSelectProduct, showProductCardButtons }) => {
-    return (
-        <Grid container className="product-grid-container" sx={{ marginTop: '20px', width: '80%', margin: '0 auto', marginBottom: '20px', background: '#CDD1DE' }}>
-            {products.map(product => (
-                <Grid key={product.id} item xs={12} sm={6} md={2} lg={2} style={{ display: 'flex' }}>
-                    {/* Passe a função addToCart como propriedade para o componente ProductCard */}
-                    <ProductCard product={product} addToCart={addToCart} onSelectProduct={onSelectProduct} showButtons={showProductCardButtons}
-                    />
-                </Grid>
-            ))}
+const ProductGrid = ({
+  products,
+  addToCart,
+  onSelectProduct,
+  showProductCardButtons,
+}) => {
+  const isMobile = useMediaQuery(breakPoints.mobile);
+
+  return (
+    <Grid
+      container
+      className="product-grid-container"
+      sx={{
+        marginTop: "20px",
+        width: "80%",
+        margin: "0 auto",
+        marginBottom: "20px",
+        background: "#CDD1DE",
+      }}
+    >
+      {products.map((product) => (
+        <Grid
+          key={product.id}
+          item
+          xs={isMobile ? 6 : 12}
+          sm={6}
+          md={2}
+          lg={2}
+          style={{ display: "flex" }}
+        >
+          {/* Passe a função addToCart como propriedade para o componente ProductCard */}
+          <ProductCard
+            product={product}
+            addToCart={addToCart}
+            onSelectProduct={onSelectProduct}
+            showButtons={showProductCardButtons}
+          />
         </Grid>
-    );
+      ))}
+    </Grid>
+  );
 };
 
 export default ProductGrid;
-
