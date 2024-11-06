3569d1ee7776ce44116548509422405ef3d5b052
João Martins <jvhotmart0@gmail.com>
2024-11-06 15:03:35 -0300

    update database users (2)

--- login/database.js
+++ login/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("nosso_lar_suburbano", "root", "Bl@ck100305", {
-  host: "localhost",
+const sequelize = new Sequelize("nosso_lar_products", "admin", "53972029837", {
+  host: "nossolardb.clqm8i0mo85q.sa-east-1.rds.amazonaws.com",
   dialect: "mysql",
 });
 
